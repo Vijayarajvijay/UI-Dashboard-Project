35d1c439a52e393a18702af71d84f44856552915
Vijayrajvijay <vr104555@gmail.com>
2023-09-28 15:00:56 +0530

    created

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ function sidebar() {
             
            <li className="nav-item active ">
                 <a className="nav-link" href="index.html">
-                   <img  src="../components/images/dashboard icon.svg" alt="dashboard icon" /> &nbsp;  &nbsp;
+                   <img  src="./images/dashboard icon.svg" alt="dashboard icon" /> &nbsp;  &nbsp;
                        <span className='text-primary'>Dashboard</span></a>
             </li>
 
